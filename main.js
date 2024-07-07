06faf9ba9d303387697f5d29f2d59bcd566d8209
nakamura <hrm.nkmr04@gmail.com>
2024-07-08 07:37:56 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var boolean = true;
+if (boolean) {
+	console.log(boolean);
+};
+var num = 100
+if (num == 100) {
+	console.log(num);
+}
+console.log(num == 100);
+var text = "JavaScript"
+if (text === "JavaScript") {
+	console.log(text);
+}
